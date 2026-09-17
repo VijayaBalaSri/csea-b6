d7e0199a3bc138127ccf3ec3725492c81dced821
VijayaBalaSri <vijayabalasri01@gmail.com>
2026-09-17 12:33:44 +0530

    port no

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 
 app.use("/s",Router)
-const port = 4000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`server is running at http://localhost:${port}`);
 });
